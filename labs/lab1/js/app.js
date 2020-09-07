51588f77aa8b3004c8b08c1a86b67c20e9851828
abzara20 <azaragozapalos@gmail.com>
2020-09-07 12:57:28 -0400

    set up app.js classes and p5 functions

---
+++ labs/lab1/js/app.js
@@ -0,0 +1,24 @@
+//Abril Zaragoza-Palos Ground Rain lab
+//N320 Lab 1: Ground Rain
+//9.7.2020
+
+class raindrop {
+    constructor(){
+
+ }
+}
+
+class ground {
+    constructor(){
+    
+    }
+}
+
+function setup(){
+    createCanvas(800,600);
+    
+}
+
+function draw(){
+    background(117,133,142);
+}
