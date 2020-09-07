01da0d75c2d9241118969726a918e137704a46e3
abzara20 <azaragozapalos@gmail.com>
2020-09-07 14:17:38 -0400

    complete constructor and create update methods in raindrop class

--- labs/lab1/js/app.js
+++ labs/lab1/js/app.js
@@ -2,10 +2,23 @@
 //N320 Lab 1: Ground Rain
 //9.7.2020
 
-class raindrop {
-    constructor(){
+//array to store the raindrops in
+let rainArray = [];
 
+//class to create and move the raindrops
+class raindrop {
+    //constructor class
+    constructor(xPos,yPos){
+        this.X = xPos;
+        this.Y = yPos;
+        this.length = 15;
+        this.width = 25;
+        this.color = "(0%,0%,100%)";
  }
+    //update class that moves the raindrop
+    update(){
+    
+    }
 }
 
 class ground {
