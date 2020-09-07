3b82c4098aa2b9e210785023e18c26f4f17deaf5
abzara20 <azaragozapalos@gmail.com>
2020-09-07 15:48:39 -0400

    Update app.js

--- labs/lab1/js/app.js
+++ labs/lab1/js/app.js
@@ -2,9 +2,6 @@
 //N320 Lab 1: Ground Rain
 //9.7.2020
 
-//array to store the raindrops in
-let rainArray = [];
-
 //class to create and move the raindrops
 class raindrop {
     //constructor class
@@ -21,15 +18,27 @@ class raindrop {
     }
 }
 
-class ground {
+class Ground {
     //constructor for the ground class
     constructor( xPos, yPos, height, width){
         this.x = xPos;
         this.y = yPos;
         this.height = height;
         this.width = width;
+        this.color = 5;
+    }
+    
+    //update function for creating and changing the color of the ground
+    update(){
+        fill('rgb(30%,10%,' + this.color + '%)');
+        rect(this.x,this.y,this.height,this.width);
     }
 }
+
+//array to store the raindrops in
+let rainArray = [];
+
+let floor = new Ground(0,500,800,200);
 
 function setup(){
     createCanvas(800,600);
@@ -37,4 +46,6 @@ function setup(){
 
 function draw(){
     background(117,133,142);
+
+    floor.update();
 }
