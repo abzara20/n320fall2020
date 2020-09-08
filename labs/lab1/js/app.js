eb9941767c44b5ae5fb033ad8d07d2eb7bbea974
abzara20 <azaragozapalos@gmail.com>
2020-09-08 09:23:50 -0400

    update raindrop class and ground class
    completed update functions for both classes to count raindrop hits and alter the blue value of the ground class after 10 raindrop hits

--- labs/lab1/js/app.js
+++ labs/lab1/js/app.js
@@ -6,45 +6,60 @@
 class raindrop {
     //constructor class
     constructor(xPos,yPos){
-        this.X = xPos;
-        this.Y = yPos;
+        this.x = xPos;
+        this.y = yPos;
         this.length = 15;
         this.width = 5;
         this.color = "(0%,0%,100%)";
+        this.speed = random(1,4);
  }
     //update class that moves the raindrop
     update(){
-     ellipse(this.X, this.Y, this.width, this.length);
+     ellipse(this.x, this.y, this.width, this.length);
+    
+     this.y = this.y + this.speed
+
+     if (this.y >= 500){
+         this.y = 0;
+         hitCount = hitCount + 1;
+     }
+     return hitCount
     }
 }
 
 class Ground {
     //constructor for the ground class
-    constructor( xPos, yPos, height, width){
+    constructor( xPos, yPos, height, width, color){
         this.x = xPos;
         this.y = yPos;
         this.height = height;
         this.width = width;
-        this.color = 5;
+        this.color = color;
     }
     
     //update function for creating and changing the color of the ground
     update(){
+
+        if (hitCount>=10 && hitCount % 10 == 0){
+            this.color = this.color + 1;
+            console.log(this.color)
+        }
+
         fill('rgb(30%,10%,' + this.color + '%)');
         rect(this.x,this.y,this.height,this.width);
-        
     }
 }
 
 //array to store the raindrops in
 let rainArray = [];
-
-let floor = new Ground(0,500,800,200);
+let hitCount = 0;
+let blueVal = 5;
+let floor = new Ground(0,500,800,200, blueVal);
 
 function setup(){
     createCanvas(800,600);
     for (i=0; i<25;i++) {
-        rainArray[i] = new raindrop(random(0,800), 15) 
+        rainArray[i] = new raindrop(random(0,800), 0) 
     }
 }
 
