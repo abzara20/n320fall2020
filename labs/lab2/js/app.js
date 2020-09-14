e468c912dbd4c3f0eda4daf05fa9de3261574162
abzara20 <azaragozapalos@gmail.com>
2020-09-13 22:15:35 -0400

    create instrument classes in app.js

---
+++ labs/lab2/js/app.js
@@ -0,0 +1,22 @@
+class instrument{
+    constructor(loudness, family, verb){
+
+    }
+
+    //function to play the instrument
+    play () {
+        
+    }
+}
+
+class woodwind{
+
+}
+
+class percussion{
+
+}
+
+class string{
+
+}
