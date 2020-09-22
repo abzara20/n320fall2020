d5114927d87de41249c8586b0b029c63029f070d
abzara20 <azaragozapalos@gmail.com>
2020-09-21 21:39:34 -0400

    set up classes and basic html for lab

---
+++ labs/lab3/js/app.js
@@ -0,0 +1,20 @@
+let candyOne = {
+    name = "Twix",
+    stock = 14
+}
+let candyTwo = {
+    name = "KitKat",
+    stock = 30
+}
+let chips = {
+    name = "SunChips",
+    stock = 28
+} 
+
+function vendIten(){
+
+};
+function render() {
+
+
+};
