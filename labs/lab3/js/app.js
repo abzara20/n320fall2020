6f7a5887a0552cd0307b8e74907964cd274ccb53
abzara20 <azaragozapalos@gmail.com>
2020-09-21 22:26:12 -0400

    set up constructor and render classes

--- labs/lab3/js/app.js
+++ labs/lab3/js/app.js
@@ -11,10 +11,29 @@ let chips = {
     stock = 28
 } 
 
-function vendIten(){
+class vendingMachine{
+    constructor( c1, c2, chip, stock1, stock2, stock3){
+        this.candyOne = c1;
+        this.candyTwo = c2;
+        this.chip = chip;
+        this.c1stock = stock1;
+        this.c2stock = stock2;
+        this.c3stock = stock3;
+    }
+    vend(){
 
-};
-function render() {
-
-
+    }
+    render() {
+        return `
+        <div>
+        Product: ${this.candyOne} Stock: ${this.c1stock}
+        </div>
+        <div>
+        Product: ${this.candyTwo} Stock: ${this.c2stock}
+        </div>
+        <div>
+        Product: ${this.chip} Stock: ${this.c3stock}
+        </div>
+        `
+    }
 };
