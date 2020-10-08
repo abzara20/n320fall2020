9052d197737ec60357ee227fee036d6d5777d409
abzara20 <azaragozapalos@gmail.com>
2020-10-08 16:53:37 -0400

    animate the wrapper to make the mockup show

---
+++ labs/lab5/js/app.js
@@ -0,0 +1,8 @@
+
+let wrapper = document.getElementById("wrapper");
+
+TweenMax.from(wrapper, {duration:2.5, alpha:0, y: 200});
+
+let banner = document.getElementById("banner");
+// console.log(banner);
+
