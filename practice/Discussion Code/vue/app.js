165f1f86320fcfa2f43fc8b4a843e55a35b4b32d
abzara20 <azaragozapalos@gmail.com>
2020-10-15 13:54:33 -0400

    completed practices and linked js files in lab 6

--- practice/Discussion Code/vue/app.js
+++ practice/Discussion Code/vue/app.js
@@ -1,12 +1,13 @@
 Vue.component("discussion-view", {
-
-
+props:["message"],
+template:"<p>here is a message for you: {{message}}<p>"
 })
 
 let app = new Vue ({
 el: "#app",
 data: {
     words: "I'm well, thanks for asking!",
-    greetings: ["Buenos Tardes", "Good Afternoon", "こんにちわ"]
+    greetings: ["Buenos Tardes", "Good Afternoon", "こんにちわ"],
+    message: "hey, what's poppin?"   
 }
 });
