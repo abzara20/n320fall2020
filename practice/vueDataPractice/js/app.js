5abefd2c736e94694bb43879d0d0b91a4280bee8
abzara20 <azaragozapalos@gmail.com>
2020-10-25 23:06:27 -0400

    create notes for json and vue

---
+++ practice/vueDataPractice/js/app.js
@@ -0,0 +1,13 @@
+axios.get('data/data.json')
+    .then(function (response){
+        // handle success
+        console.log(response);
+    })
+
+let app = new Vue({
+    el: "#app",
+    data: { 
+    },
+    methods: {
+    }   
+});
