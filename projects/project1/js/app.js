73843911cc483a5117c8884897c4d1f83d9a27b6
abzara20 <azaragozapalos@gmail.com>
2020-10-26 21:00:29 -0400

    working on player turn

--- projects/project1/js/app.js
+++ projects/project1/js/app.js
@@ -1,13 +1,34 @@
-//retrieve the squares for the tic tac toe grid and store into an array
-let svgVar = document.getElementsByClassName("cls-2");
-//for loop for every part of the array to add an event listener
-for ( i = 0; i < svgVar.length; i++) {
-//adding an event listener in order to add js 
-svgVar[i].addEventListener("click", onCLick);
+class game{
+    constructor(){
+        this.playerTurn = 1;
+        this.svgVar = document.getElementsByClassName("cls-2");
+        this.boardArray = [
+            [0,0,0],
+            [0,0,0],
+            [0,0,0]
+        ]
+    }
+    turn(){
+        //for loop for every part of the array to add an event listener
+        for (let i = 0; i < this.svgVar.length; i++) {   
+            //adding an event listener in order to add js 
+            this.svgVar[i].addEventListener("click", function(event){
+
+                if (event.target.getAttribute("select") == 0) {
+
+                    if (this.playerTurn % 2 == 0){
+                        event.target.style.fill = "blue";
+                        this.playerTurn = this.playerTurn + 1;
+                    } else {
+                        event.target.style.fill = "pink";
+                        this.playerTurn ++;
+                    }
+                }  
+            }, false);
+        }
+    }
 }
 
-//function that triggers everytime a person clicks on a square
-function onCLick(event){
-    //accessing the css file and changing the color of the square
-    event.target.style.fill = "blue" ;
-}
+
+let play = new game;
+play.turn();
